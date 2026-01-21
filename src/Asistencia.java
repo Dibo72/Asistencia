f79d1a93bd328f127bc6aef030cea6b5eaadef3a
Dibo72 <humanida133@gmail.com>
2026-01-21 13:10:33 +0100

    Ejercicio comentado

--- src/Asistencia.java
+++ src/Asistencia.java
@@ -1,15 +1,26 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashSet;
+public class Asistencia {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        List<String> listaAsistencia = new ArrayList<>();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        listaAsistencia.add("Juan Pérez");
+        listaAsistencia.add("María García");
+        listaAsistencia.add("Carlos Ruiz");
+        listaAsistencia.add("Juan Pérez");
+        listaAsistencia.add("Ana López");
+        listaAsistencia.add("María García");
+        listaAsistencia.add("Luis Torres");
+        listaAsistencia.add("Juan Pérez");
+        listaAsistencia.add("Elena Gómez");
+        listaAsistencia.add("Carlos Ruiz");
+
+        System.out.println("Lista con el problema: " + listaAsistencia);
+//Cambiamos la lista a HashSet para eliminar los elementos duplicados
+        HashSet<String> listaSinDuplicados = new HashSet<>(listaAsistencia);
+
+        System.out.println("Lista sin el problema: " + listaSinDuplicados);
+
     }
 }
